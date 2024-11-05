375f0ba7ff6759a89490921b6a9b6d7f6956f8a6
Сулейман Хасанов <macbook@MacBook-Air.local>
2024-11-05 20:42:52 +0500

    Aded autoload movies

--- src/features/genreFilter/utils/hooks/useFilterMoviesByGenre.js
+++ src/features/genreFilter/utils/hooks/useFilterMoviesByGenre.js
@@ -4,24 +4,45 @@ import { useSelector } from 'react-redux';
 
 const useFilterMoviesByGenre = () => {
   const genreId = useSelector((state) => state.filter);
-
+  const [page, setPage] = useState(1);
   const [movies, setMovies] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
 
   useEffect(() => {
     const fetchMovies = async () => {
       if (!genreId) return;
+      setIsLoading(true);
       try {
-        const fetchedMovies = await getMoviesByGenre(genreId);
-        setMovies(fetchedMovies);
+        const fetchedMovies = await getMoviesByGenre(genreId, page);
+
+        // Исключение фильмов с genre_id 16
+        const filteredMovies = fetchedMovies.filter(
+          (movie) => !movie.genre_ids.includes(16),
+        );
+
+        setMovies((prevMovies) => {
+          const uniqueMovies = filteredMovies.filter(
+            (newMovie) =>
+              !prevMovies.some((movie) => movie.id === newMovie.id),
+          );
+          return [...prevMovies, ...uniqueMovies];
+        });
       } catch (error) {
         console.error('Ошибка при загрузке фильмов:', error);
+      } finally {
+        setIsLoading(false);
       }
     };
 
     fetchMovies();
+  }, [genreId, page]);
+
+  useEffect(() => {
+    setPage(1);
+    setMovies([]);
   }, [genreId]);
 
-  return movies;
+  return { movies, isLoading, setPage };
 };
 
 export default useFilterMoviesByGenre;
